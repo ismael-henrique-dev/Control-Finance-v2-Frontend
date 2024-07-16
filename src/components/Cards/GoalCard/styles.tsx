92a7bfd39f3469f32e764014170c57853316daed
Ismael <ismael.henrique.dev@gmail.com>
2024-07-16 17:44:05 -0300

    Home Summary created

--- src/components/Cards/GoalCard/styles.tsx
+++ src/components/Cards/GoalCard/styles.tsx
@@ -62,3 +62,71 @@ export const BarLinearProgress = styled(LinearProgress)`
       props.theme.secundaryGray}; /* cor do fundo da barra */
   }
 `
+
+export const HomeSummary = styled.div`
+width: 20rem;
+  border-radius: 12px;
+  background-color: ${(props) => props.theme.primaryGray};
+  display: flex;
+  justify-content: center;
+  align-items: start;
+  flex-direction: column;
+  padding: 1.5rem;
+  gap: 1.5rem;
+  
+  section {
+    display: flex;
+    flex-direction: column;
+    gap: 1.75rem;
+  }
+`
+
+export const MainBalance = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: auto;
+
+  strong {
+    color: ${(props) => props.theme.secundary};
+    font-weight: 600;
+    font-size: 2rem;
+  }
+
+  span {
+    color: ${(props) => props.theme.terciaryGray};
+    font-weight: 600;
+    font-size: 0.875rem;
+  }
+`
+
+interface TypeTransactionIconProps {
+  variant: "income" | "outcome"
+}
+
+export const TypeTransaction = styled.div<TypeTransactionIconProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.75rem;
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 2.5rem;
+    height: 2.5rem;
+    border-radius: 50%;
+    background-color: ${(props) =>
+      props.variant === "income" ? props.theme.green : props.theme.red};
+    color: ${(props) => props.theme.white};
+  }
+
+  span {
+    color: ${(props) =>
+      props.variant === "income" ? props.theme.green : props.theme.red};
+    font-weight: 600;
+    font-size: 1.25rem;
+  }
+`
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,12 +2,36 @@ import { NavLink } from "react-router-dom";
 import { ContainerDefault, HomeContainer, TopContainer } from "./styles";
 import { AccountCard } from "../../components/Cards/AccountCard";
 import { GoalCard } from "../../components/Cards/GoalCard";
+import { HomeSummary, MainBalance, TypeTransaction } from "../../components/Cards/GoalCard/styles";
+import { ArrowDown, ArrowUp } from "lucide-react";
 
 export function Home() {
 
   return (
     <HomeContainer>
-      <ContainerDefault></ContainerDefault>
+      <ContainerDefault>
+        <HomeSummary>
+          <MainBalance>
+            <strong>R$ 400,00</strong>
+            <span>Saldo atual</span>
+          </MainBalance>
+          <section>
+            <TypeTransaction variant="income">
+              <div>
+                <ArrowUp />
+              </div>
+              <span>R$ 600,00</span>
+            </TypeTransaction>
+
+            <TypeTransaction variant="outcome">
+              <div>
+                <ArrowDown />
+              </div>
+              <span>R$ 200,00</span>
+            </TypeTransaction>
+          </section>
+        </HomeSummary>
+      </ContainerDefault>
 
       <ContainerDefault>
         <TopContainer>
