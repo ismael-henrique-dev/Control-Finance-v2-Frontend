45febaea4454d89f18df55b4ff6ce4d3f0902d10
Ismael <ismael.henrique.dev@gmail.com>
2024-09-21 22:05:00 -0300

    presentaions drawer fix style

--- src/components/Header/Drawer/index.tsx
+++ src/components/Header/Drawer/index.tsx
@@ -42,37 +42,39 @@ export function DrawerBasic() {
           onClick={toggleDrawer(false)}
           onKeyDown={toggleDrawer(false)}
         >
-          <PresentationSection>
-            <img src={testImage} />
-            <span>Bem-vindo(a) novamente!</span>
-            <strong>{userData?.UsernName}</strong>
-          </PresentationSection>
-          <ListContainer>
-            <ListItem>
-              <PieChart />
-              <NavLink to="/">Dashboard</NavLink>
-            </ListItem>
-            <ListItem>
-              <ArrowRightLeft />
-              <NavLink to="/transacoes">Transações</NavLink>
-            </ListItem>
-            <ListItem>
-              <PiggyBank />
-              <NavLink to="/contas">Contas</NavLink>
-            </ListItem>
-            <ListItem>
-              <BarChart />
-              <NavLink to="/rendimento">Rendimento</NavLink>
-            </ListItem>
-            <ListItem>
-              <Goal />
-              <NavLink to="/metas">Metas</NavLink>
-            </ListItem>
-            <ListItem>
-              <LogOut />
-              <NavLink onClick={userLogout} to="/login">Log-out</NavLink>
-            </ListItem>
-          </ListContainer>
+          <div>
+            <PresentationSection>
+              <img src={testImage} />
+              <span>Bem-vindo(a) novamente!</span>
+              <strong>{userData?.UsernName}</strong>
+            </PresentationSection>
+            <ListContainer>
+              <ListItem>
+                <PieChart />
+                <NavLink to="/">Dashboard</NavLink>
+              </ListItem>
+              <ListItem>
+                <ArrowRightLeft />
+                <NavLink to="/transacoes">Transações</NavLink>
+              </ListItem>
+              <ListItem>
+                <PiggyBank />
+                <NavLink to="/contas">Contas</NavLink>
+              </ListItem>
+              <ListItem>
+                <BarChart />
+                <NavLink to="/rendimento">Rendimento</NavLink>
+              </ListItem>
+              <ListItem>
+                <Goal />
+                <NavLink to="/metas">Metas</NavLink>
+              </ListItem>
+            </ListContainer>
+          </div>
+          <button onClick={userLogout}>
+            <LogOut />
+            <span>Log-out</span>
+          </button>
         </ContainerDrawer>
       </Drawer>
     </Box>
