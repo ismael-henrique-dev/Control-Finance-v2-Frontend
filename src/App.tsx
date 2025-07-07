e847ca6acfd9b45274f08cd88f91ab1f1f52dadd
Ismael <ismael.henrique.dev@gmail.com>
2025-07-07 14:24:06 -0300

    feat: add react-query and update API configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useContext } from "react"
-import { ThemeProvider as StyledThemeProvider } from "styled-components"
-import { GlobalStyle } from "./styles/global"
-import { lightTheme } from "./styles/themes/light"
-import { darkTheme } from "./styles/themes/dark"
-import { BrowserRouter } from "react-router-dom"
-import { Router } from "./routes/Router"
+import { useContext } from 'react'
+import { ThemeProvider as StyledThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { lightTheme } from './styles/themes/light'
+import { darkTheme } from './styles/themes/dark'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './routes/Router'
 import {
   ThemeProvider,
   ThemeContext,
@@ -12,12 +12,14 @@ import {
   TransactionsProvider,
   GoalsProvider,
   UseProvider,
-} from "./contexts"
-import { GoogleOAuthProvider } from "@react-oauth/google"
+} from './contexts'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+const queryClient = new QueryClient()
 
 function App() {
   const { theme } = useContext(ThemeContext)
-  const currentTheme = theme === "light" ? lightTheme : darkTheme
+  const currentTheme = theme === 'light' ? lightTheme : darkTheme
 
   return (
     <StyledThemeProvider theme={currentTheme}>
@@ -27,24 +29,27 @@ function App() {
   )
 }
 
-const clientID = "400143903483-lenkce076k19ikl97v5d1hb4isb011rd.apps.googleusercontent.com"
+const clientID =
+  '400143903483-lenkce076k19ikl97v5d1hb4isb011rd.apps.googleusercontent.com'
 
 export default function MainApp() {
   return (
-    <ThemeProvider>
-      <BrowserRouter>
-        <AccountsProvider>
-          <TransactionsProvider>
-            <GoalsProvider>
-              <UseProvider>
-                <GoogleOAuthProvider clientId={clientID}>
-                  <App />
-                </GoogleOAuthProvider>
-              </UseProvider>
-            </GoalsProvider>
-          </TransactionsProvider>
-        </AccountsProvider>
-      </BrowserRouter>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <BrowserRouter>
+          <AccountsProvider>
+            <TransactionsProvider>
+              <GoalsProvider>
+                <UseProvider>
+                  <GoogleOAuthProvider clientId={clientID}>
+                    <App />
+                  </GoogleOAuthProvider>
+                </UseProvider>
+              </GoalsProvider>
+            </TransactionsProvider>
+          </AccountsProvider>
+        </BrowserRouter>
+      </ThemeProvider>
+    </QueryClientProvider>
   )
 }
--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,5 +1,7 @@
-import axios from "axios";
+import axios from 'axios'
+
+const apiUrl = import.meta.env.VITE_API_URL
 
 export const api = axios.create({
-  baseURL: "https://control-finance-v2-api.onrender.com",
+  baseURL: apiUrl,
 })
