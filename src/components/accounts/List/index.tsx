f5676fa48569d3593b76a20c931765a4c965a51c
Ismael <ismael.henrique.dev@gmail.com>
2025-07-29 09:42:49 -0300

    feat(accounts): delete account

--- src/components/accounts/List/index.tsx
+++ src/components/accounts/List/index.tsx
@@ -1,3 +1,8 @@
+import { deleteAccountById } from '@/services/http/account/DeleteAccount'
+import { getErrorMessage } from '@/utils/GetErrorMessage'
+import { useQueryClient } from '@tanstack/react-query'
+import { toast } from 'sonner'
+
 type AccountsListProps = {
   accounts: Account[]
   isLoading: boolean
@@ -5,7 +10,14 @@ type AccountsListProps = {
   errorMessage?: string
 }
 
-export function AccountsList({ accounts, isLoading, isError, errorMessage }: AccountsListProps) {
+export function AccountsList({
+  accounts,
+  isLoading,
+  isError,
+  errorMessage,
+}: AccountsListProps) {
+  const queryClient = useQueryClient()
+
   if (isLoading) {
     return <p>Carregando contas...</p>
   }
@@ -14,18 +26,30 @@ export function AccountsList({ accounts, isLoading, isError, errorMessage }: Acc
     return <p>Erro ao carregar as contas: {errorMessage}</p>
   }
 
-  // const handleDeleteAccount = () => {
+  const handleDeleteAccount = async (id: string) => {
+    try {
+      await deleteAccountById(id)
 
-  // }
+      queryClient.invalidateQueries({ queryKey: ['accounts'] })
+
+      toast.success('Conta deletada com êxito.')
+    } catch (error) {
+      const errorMessage = getErrorMessage(error)
+
+      toast.error(errorMessage)
+    }
+  }
 
   return (
     <div>
       {accounts.length > 0 ? (
         <ul>
-          {accounts.map(account => (
+          {accounts.map((account) => (
             <li key={account.id}>
               {account.title} - Saldo: R$ {account.value.toFixed(2)}
-              {/* <button onClick={}></button> */}
+              <button onClick={() => handleDeleteAccount(account.id)}>
+                Excluir
+              </button>
             </li>
           ))}
         </ul>
